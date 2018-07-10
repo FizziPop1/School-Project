5b671fa3542b7b420c4a8e603b75c3e788153c38
FizziPop1 <41065522+FizziPop1@users.noreply.github.com>
2018-07-10 18:43:34 +0100

    Update menu.py

--- LICENSE.md/menu.py
+++ LICENSE.md/menu.py
@@ -12,14 +12,15 @@ quit =tkinter.Button(text="QUIT", fg="red", command=quit, height=4, width=6)
 quit.place(x=400, y=400) 
 
 def new_win(): # new window definition
-    new = tkinter.TK()
+    new = tkinter.Tk()
     new.title("New Window")
     new.geometry("800x800")
     new.configure(background='white')
     new.resizable(width=False, height=False)
     new.mainloop()
-    close = tkinter.Button(text="QUIT", fg="red", command=quit, height=4, width=6)
-    close.place(x=400, y=400)
+
+close = tkinter.Button(text="QUIT", fg="red", command=quit, height=4, width=6)
+close.place(x=400, y = 500)
     
     
 window=tkinter.Button(text="open new window", fg="blue", command=new_win, height=4, width=6) #command linked
