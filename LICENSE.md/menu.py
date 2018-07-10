314dbf0afe2f2f573f150f1afaa1d6c80dc0b34d
FizziPop1 <41065522+FizziPop1@users.noreply.github.com>
2018-07-10 21:57:37 +0100

    Update menu.py

--- LICENSE.md/menu.py
+++ LICENSE.md/menu.py
@@ -1,28 +1,34 @@
-import tkinter                   
+from tkinter import *
 
-
-
-root = tkinter.Tk()
+root = Tk()
 root.title("OCR Revision Tool")
 root.geometry("800x800")
 root.configure(background='white')
 root.resizable(width=False, height=False)
 
-close=tkinter.Button(text="QUIT", fg="red", command=quit, height=4, width=6)
+close = Button(text="QUIT", fg="red", command=quit, height=4, width=6)
 close.place(x=400, y=400) 
 
 def new_win(): # new window definition
-    new = tkinter.Tk()
-    new.title("New Window")
-    new.geometry("800x800")
-    new.configure(background='white')
-    new.resizable(width=False, height=False)
-    new.mainloop()
-    close = tkinter.Button(text="QUIT", fg="red", command=quit, height=4, width=6)
-    close.place(x=400, y = 400)
+
+    about = Tk()
+    about.title("About")
+    about.geometry("300x500")
+    about.configure(background='white')
+    about.resizable(width=False, height=False)
     
+    about_text = Text(root, height=4, width= 50)
+    about_text.pack()
+    about_text.insert(END, "OCR AS Level Computer Science Quiz")
     
-window=tkinter.Button(text="New Window", fg="blue", command=new_win, height=4, width=12) #command linked
+    about.mainloop()
+
+close = Button(text="QUIT", fg="red", command=quit, height=4, width=6)
+close.place(x=400, y = 400)
+
+window = Button(text="New Window", fg="blue", command=new_win, height=4, width=12) #command linked
 window.place(x=400, y=350)
+
+
 
 root.mainloop()
