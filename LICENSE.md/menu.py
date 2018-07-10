c390002b6f5e3ff507503e03e33db61650b1a315
Joshua Myers <myers.joshua.281@gmail.com>
2018-07-10 10:34:35 -0400

    try that

--- LICENSE.md/menu.py
+++ LICENSE.md/menu.py
@@ -11,19 +11,17 @@ root.resizable(width=False, height=False)
 quit = tkinter.Button(text="QUIT", fg="red", command=quit, height = 4, width = 6)
 quit.place(x=400, y=400) 
 
-
 def new_winF(): # new window definition
-    newwin = Toplevel(root)
-    display = Label(newwin, text="Humm, see a new window !")
-    display.pack()    
-
+    display = Label(Toplevel(root), text="Title")
+    new = tkinter.TK()
+    new.title("New Window")
+    new.geometry("800x800")
+    new.configure(background='white')
+    new.resizable(width=False, height=False)
+    new.mainloop()
 
 button1 = tkinter.Button(root, text ="open new window", command =new_winF) #command linked
 button1.pack()
-
-
-
-
 
 
 
